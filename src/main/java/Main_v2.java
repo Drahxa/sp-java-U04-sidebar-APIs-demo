659014bbaea37f87bc301a13f7cd55d1f66a3775
sp1dev <spaulino.dev+github@gmail.com>
2024-05-21 10:19:59 -0400

    updated description of Main_v4

--- src/main/java/Main_v2.java
+++ src/main/java/Main_v2.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 // Main_v1: the API response is returned as a string.
 // Main_v2: the GSON package is used to Pretty print the API response.
 // Main_v3: the GSON package is used to parse through the API response.
-// Main_v4: a Fruit class is utilized
+// Main_v4: a Fruit class is utilized to store a fruit. The FruitService class handles the API call and response.
 public class Main_v2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
